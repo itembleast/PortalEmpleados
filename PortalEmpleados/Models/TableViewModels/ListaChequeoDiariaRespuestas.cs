672a923bf19eb673381b5c5174e2ffa8ed879fa0
Juan Dominguez <jdominguez@LAASCENSION.COM.CO>
2023-02-08 17:51:58 -0500

    Add project files.

---
+++ PortalEmpleados/Models/TableViewModels/ListaChequeoDiariaRespuestas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalEmpleados.Models.TableViewModels
+{
+    public class ListaChequeoDiariaRespuestas
+    {
+    }
+}
